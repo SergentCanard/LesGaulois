873687242d32ccabe02ea48eaa09a14b21584394
SergentCanard <maximeguillou05@gmail.com>
2024-02-15 18:25:05 +0100

    continuation du tp1 jusque page 10

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,7 +1,19 @@
 package personnages;
 
 public class Gaulois {
+	
 	private String nom;
 	private int force;
-	private int effetPotion;
+	private int effetPotion = 1;
+	
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	//page 10 du diapo Tp1
 }
